a4c11d8e550f791091b3274b1bcca2bd05bc1f5c
Mira <lucas-mira1@hotmail.com>
2024-09-12 15:00:11 -0300

    project created

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col justify-center items-center w-screen h-screen">
+        <p class="heading-m flex gap-2">
+            <img class="w-15" src="~/assets/logo-devlinks-small.svg" alt="">
+            My-Link
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage'
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
